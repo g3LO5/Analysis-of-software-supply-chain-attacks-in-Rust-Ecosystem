6d1f7b176efff5e1943859a8d9fdc98647351305
g3LO5 <75740255+g3LO5@users.noreply.github.com>
2023-12-05 22:13:10 +0700

    update results anylze file

--- package/Detect/Analyze_file.py
+++ package/Detect/Analyze_file.py
@@ -6,7 +6,7 @@ import re
 folder_path = r'C:\Users\Admin\OneDrive\Documents\GitHub\Analysis-of-software-supply-chain-attacks-in-Rust-Ecosystem\package\Detect\result'
 
 # Mở file .csv để ghi kết quả
-with open('results.csv', 'w', newline='', encoding='utf-8') as csvfile:
+with open('results_analy_file.csv', 'w', newline='', encoding='utf-8') as csvfile:
     fieldnames = ['package_name', 'total_tags', 'unique_tags']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
@@ -16,10 +16,18 @@ with open('results.csv', 'w', newline='', encoding='utf-8') as csvfile:
         if filename.endswith('.txt'):
             with open(os.path.join(folder_path, filename), 'r', encoding='utf-8') as f:
                 content = f.read()
+                # Loại bỏ các ký tự màu sắc
+                content = re.sub(r'\x1b\[[0-9;]*m', '', content)
                 package_name = filename.replace('.txt', '')
-                total_tags = int(re.search(r'Match #1 of (\d+)', content).group(1))
-                tags = re.findall(r'Tag: (.+)', content)
-                unique_tags = list(set(tags))  # Loại bỏ các tag trùng lặp
+                if '0 matches found.' in content:
+                    # Nếu không tìm thấy kết quả phù hợp, ghi kết quả vào file .csv với total_tags là 0 và không có unique_tags
+                    writer.writerow({'package_name': package_name, 'total_tags': 0, 'unique_tags': ''})
+                else:
+                    match = re.search(r'Match #1 of (\d+)', content)
+                    if match is not None:
+                        total_tags = int(match.group(1))
+                        tags = re.findall(r'Tag: (.+)', content)
+                        unique_tags = list(set(tags))  # Loại bỏ các tag trùng lặp
 
-                # Ghi kết quả vào file .csv ngay lập tức
-                writer.writerow({'package_name': package_name, 'total_tags': total_tags, 'unique_tags': ', '.join(unique_tags)})
+                        # Ghi kết quả vào file .csv ngay lập tức
+                        writer.writerow({'package_name': package_name, 'total_tags': total_tags, 'unique_tags': ', '.join(unique_tags)})
