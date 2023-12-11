f43539454ef187029ac85cfcb7f133f5d3d71ad4
g3LO5 <hao3052002@gmail.com>
2023-12-11 19:32:36 +0700

    edit image csv and update it

--- package/Detect/Analyze_file.py
+++ package/Detect/Analyze_file.py
@@ -19,15 +19,15 @@ with open('results_analy_file.csv', 'w', newline='', encoding='utf-8') as csvfil
                 # Loại bỏ các ký tự màu sắc
                 content = re.sub(r'\x1b\[[0-9;]*m', '', content)
                 package_name = filename.replace('.txt', '')
-                if '0 matches found.' in content:
-                    # Nếu không tìm thấy kết quả phù hợp, ghi kết quả vào file .csv với total_tags là 0 và không có unique_tags
-                    writer.writerow({'package_name': package_name, 'total_tags': 0, 'unique_tags': ''})
-                else:
-                    match = re.search(r'Match #1 of (\d+)', content)
-                    if match is not None:
-                        total_tags = int(match.group(1))
-                        tags = re.findall(r'Tag: (.+)', content)
-                        unique_tags = list(set(tags))  # Loại bỏ các tag trùng lặp
+                match = re.search(r'Match #1 of (\d+)', content)
+                if match is not None:
+                    total_tags = int(match.group(1))
+                    tags = re.findall(r'Tag: (.+)', content)
+                    unique_tags = list(set(tags))  # Loại bỏ các tag trùng lặp
 
-                        # Ghi kết quả vào file .csv ngay lập tức
-                        writer.writerow({'package_name': package_name, 'total_tags': total_tags, 'unique_tags': ', '.join(unique_tags)})
+                    if total_tags == 0:
+                        # Nếu total_tags bằng 0, bỏ qua việc ghi kết quả vào file .csv
+                        continue
+
+                    # Ghi kết quả vào file .csv ngay lập tức
+                    writer.writerow({'package_name': package_name, 'total_tags': total_tags, 'unique_tags': ', '.join(unique_tags)})
